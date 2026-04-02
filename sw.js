cac6611cfb6a7b6788386dc3f16d53611ec2a4b0
engmehmet17-pixel <eng.mehmet17@gmail.com>
2026-04-03 02:57:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
 const CACHE_NAME = 'fuel-app-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://i.ibb.co/vz1f0b0/fuel-app-icon.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+    self.skipWaiting();
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
