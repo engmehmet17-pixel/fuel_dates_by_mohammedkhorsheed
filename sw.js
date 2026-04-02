220e80dae256c2c7adb0a4c4e01caefbdb7ca2de
engmehmet17-pixel <eng.mehmet17@gmail.com>
2026-04-03 02:00:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,23 @@
-const CACHE_NAME = 'fuel-app-v2'; // قمت بتحديث الإصدار لضمان التنشيط
-const ASSETS = [
+const CACHE_NAME = 'fuel-app-v1';
+const assets = [
   './',
   './index.html',
-  './manifest.json?v=2.0',
-  'https://i.ibb.co/vz1f0b0/fuel-app-icon.png' // رابط الأيقونة الجديدة
+  './manifest.json',
+  'https://i.ibb.co/vz1f0b0/fuel-app-icon.png'
 ];
 
-// مرحلة التثبيت: حفظ الملفات الأساسية في الكاش
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ASSETS);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-// مرحلة التنشيط: مسح الكاش القديم إذا وجد
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
-    );
-    self.clients.claim();
-});
-
-// استراتيجية جلب البيانات: الكاش أولاً ثم الشبكة (لتسريع الأداء)
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
