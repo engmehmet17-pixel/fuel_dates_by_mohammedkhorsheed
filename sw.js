9b96037f89abb52a1645c17cca42f122ebca5806
engmehmet17-pixel <eng.mehmet17@gmail.com>
2026-04-03 03:09:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = 'fuel-app-v1';
+const CACHE_NAME = 'fuel-app-v3';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/4814/4814489.png'
+];
+
 self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    );
     self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
     e.waitUntil(clients.claim());
 });
+
 self.addEventListener('fetch', e => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
