b9b0f06b64e4d9ff32bf73ba9caa60602d5d8f61
engmehmet17-pixel <eng.mehmet17@gmail.com>
2026-04-04 15:52:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
 const CACHE_NAME = 'fuel-v1';
 const assets = [
-  'index.html',
-  'manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://fonts.gstatic.com/s/e/notoemoji/latest/26fd/512.png'
 ];
 
+// التثبيت وحفظ الملفات في ذاكرة الهاتف
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
+  self.skipWaiting();
 });
 
+// تفعيل وضع "الأوفلاين التام"
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
